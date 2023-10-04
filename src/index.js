7ac15cdbeb574df072674c5fa986e73422a64692
Serhii <stupanenko.serhii@gmail.com>
2023-10-04 23:55:10 +0300

    add eventListener to buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import initialLoad from "./pageLoad";
+import { populateHomePage } from "./home";
+import { populateMenuPage } from "./menu";
+import {populateContactPage} from "./contact"
 
 initialLoad();
+
+const homeBtn = document.querySelector('.Home')
+const menuBtn = document.querySelector('.Menu')
+const contactBtn = document.querySelector('.Contact')
+homeBtn.addEventListener('click', () => {
+    document.querySelector('.main').appendChild(populateHomePage());
+})
+menuBtn.addEventListener('click', () => {
+    document.querySelector('.main').appendChild(populateMenuPage());
+})
+contactBtn.addEventListener('click', () => {
+    document.querySelector('.main').appendChild(populateContactPage());
+})
