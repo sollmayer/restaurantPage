a8a45739a923c059ab65b815d5c8d3aec42a32f9
Serhii <stupanenko.serhii@gmail.com>
2023-10-06 23:36:25 +0300

    edit home page

--- src/home.js
+++ src/home.js
@@ -1,17 +1,28 @@
+import { loadMenuPage } from "./menu"
 const createHomePage = () => {
     const homeContainer = document.createElement('div')
     const homeTitle = document.createElement('h1')
     const description = document.createElement('p')
-    description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    homeTitle.textContent = "HOME";
+    const orderBtn = document.createElement('button')
 
+    description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    homeTitle.textContent = "RESTAURANT NAME";
+    orderBtn.textContent = "Order Now";
+    orderBtn.addEventListener('click', ()=> {
+        loadMenuPage();
+    })
+    
     homeContainer.appendChild(homeTitle)
     homeContainer.appendChild(description)
-
+    homeContainer.appendChild(orderBtn)
+    homeContainer.classList.add('home');
+    
     return homeContainer;
 }
 
 export const loadHomePage = () => {
+    document.querySelectorAll('.header button').forEach(btn => btn.classList.remove('active'))
+    document.querySelector('.Home').classList.add('active');
     const mainContainer = document.querySelector('.main');
     mainContainer.textContent = "";
     mainContainer.appendChild(createHomePage());
