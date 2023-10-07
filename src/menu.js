71db4af55aeacc2f75eb4566a8ac1bd7f05a29b4
Serhii <stupanenko.serhii@gmail.com>
2023-10-07 23:04:40 +0300

    implement createDish function

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,36 @@
 const createMenuPage = () => {
     const menuContainer = document.createElement('div')
     const menuTitle = document.createElement('h1')
-    const description = document.createElement('p')
-    description.textContent = "Ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     menuTitle.textContent = "MENU";
 
     menuContainer.appendChild(menuTitle)
-    menuContainer.appendChild(description)
+    menuContainer.appendChild(createDish("Dish1", 'description1', '0.00$'))
+    menuContainer.appendChild(createDish("Dish2", 'description2', '0.00$'))
+    menuContainer.appendChild(createDish("Dish3", 'description3', '0.00$'))
+    menuContainer.appendChild(createDish("Dish4", 'description4', '0.00$'))
+    menuContainer.appendChild(createDish("Dish5", 'description5', '0.00$'))
     menuContainer.classList.add('menu');
     
     return menuContainer;
+}
+
+const createDish = (name, description, price) => {
+    const dishContainer = document.createElement('div')
+    const dishName = document.createElement('h2');
+    const dishDescription = document.createElement('p');
+    const dishPrice = document.createElement('p');
+    const dishLine = document.createElement('hr');
+    dishName.textContent = name;
+    dishDescription.textContent = description;
+    dishPrice.textContent = price;
+
+    dishContainer.classList.add('dish');
+    dishContainer.appendChild(dishName);
+    dishContainer.appendChild(dishDescription);
+    dishContainer.appendChild(dishLine);
+    dishContainer.appendChild(dishPrice);
+
+    return dishContainer;
 }
 
 export const loadMenuPage = () => {
